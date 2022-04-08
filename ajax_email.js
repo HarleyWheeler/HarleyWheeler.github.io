73993886797b2ce2063c0ab3ca1e38fcc62cb02a
HarleyW <wheel488@umn.edu>
2022-04-08 11:56:31 -0500

    gitignore

--- ajax_email.js
+++ ajax_email.js
@@ -91,7 +91,7 @@
       // bind to the submit event of our form
       var forms = document.querySelectorAll("form.gform");
       for (var i = 0; i < forms.length; i++) {
-        forms[i].addEventListener("form-submit", handleFormSubmit, false);
+        forms[i].addEventListener("submit", handleFormSubmit, false);
       }
     };
     document.addEventListener("DOMContentLoaded", loaded, false);
